773539022d809a7ef7d6d98dc1be9588a9287f3b
veoutlo <veoutlo@gmail.com>
2023-06-30 19:54:44 +0700

    added gitignore

--- build/CMakeFiles/doseDetector.dir/DependInfo.cmake
+++ build/CMakeFiles/doseDetector.dir/DependInfo.cmake
@@ -1,50 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/ask147/Documents/projects/DoseDetector/doseDetector.cc" "/home/ask147/Documents/projects/DoseDetector/build/CMakeFiles/doseDetector.dir/doseDetector.cc.o"
-  "/home/ask147/Documents/projects/DoseDetector/src/EffectiveDose.cc" "/home/ask147/Documents/projects/DoseDetector/build/CMakeFiles/doseDetector.dir/src/EffectiveDose.cc.o"
-  "/home/ask147/Documents/projects/DoseDetector/src/action.cc" "/home/ask147/Documents/projects/DoseDetector/build/CMakeFiles/doseDetector.dir/src/action.cc.o"
-  "/home/ask147/Documents/projects/DoseDetector/src/generator.cc" "/home/ask147/Documents/projects/DoseDetector/build/CMakeFiles/doseDetector.dir/src/generator.cc.o"
-  "/home/ask147/Documents/projects/DoseDetector/src/parallelWorld.cc" "/home/ask147/Documents/projects/DoseDetector/build/CMakeFiles/doseDetector.dir/src/parallelWorld.cc.o"
-  "/home/ask147/Documents/projects/DoseDetector/src/parallelWorldMany.cc" "/home/ask147/Documents/projects/DoseDetector/build/CMakeFiles/doseDetector.dir/src/parallelWorldMany.cc.o"
-  "/home/ask147/Documents/projects/DoseDetector/src/physicalConstruction.cc" "/home/ask147/Documents/projects/DoseDetector/build/CMakeFiles/doseDetector.dir/src/physicalConstruction.cc.o"
-  "/home/ask147/Documents/projects/DoseDetector/src/run.cc" "/home/ask147/Documents/projects/DoseDetector/build/CMakeFiles/doseDetector.dir/src/run.cc.o"
-  "/home/ask147/Documents/projects/DoseDetector/src/runAction.cc" "/home/ask147/Documents/projects/DoseDetector/build/CMakeFiles/doseDetector.dir/src/runAction.cc.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
-  "G4LIB_BUILD_DLL"
-  "G4UI_USE_QT"
-  "G4UI_USE_TCSH"
-  "G4VIS_USE_OPENGL"
-  "G4VIS_USE_OPENGLQT"
-  "G4VIS_USE_RAYTRACERX"
-  "G4VIS_USE_TOOLSSG_QT_GLES"
-  "PTL_BUILD_DLL"
-  "QT_CORE_LIB"
-  "QT_GUI_LIB"
-  "QT_NO_DEBUG"
-  "QT_OPENGL_LIB"
-  "QT_PRINTSUPPORT_LIB"
-  "QT_WIDGETS_LIB"
   )
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../include"
-  "/usr/local/include/Geant4"
-  "/usr/include/x86_64-linux-gnu/qt5"
-  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
-  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
-  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
-  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
-  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
-  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ask147/Documents/projects/DoseDetector/doseDetector.cc" "CMakeFiles/doseDetector.dir/doseDetector.cc.o" "gcc" "CMakeFiles/doseDetector.dir/doseDetector.cc.o.d"
+  "/home/ask147/Documents/projects/DoseDetector/src/EffectiveDose.cc" "CMakeFiles/doseDetector.dir/src/EffectiveDose.cc.o" "gcc" "CMakeFiles/doseDetector.dir/src/EffectiveDose.cc.o.d"
+  "/home/ask147/Documents/projects/DoseDetector/src/action.cc" "CMakeFiles/doseDetector.dir/src/action.cc.o" "gcc" "CMakeFiles/doseDetector.dir/src/action.cc.o.d"
+  "/home/ask147/Documents/projects/DoseDetector/src/generator.cc" "CMakeFiles/doseDetector.dir/src/generator.cc.o" "gcc" "CMakeFiles/doseDetector.dir/src/generator.cc.o.d"
+  "/home/ask147/Documents/projects/DoseDetector/src/parallelWorld.cc" "CMakeFiles/doseDetector.dir/src/parallelWorld.cc.o" "gcc" "CMakeFiles/doseDetector.dir/src/parallelWorld.cc.o.d"
+  "/home/ask147/Documents/projects/DoseDetector/src/parallelWorldMany.cc" "CMakeFiles/doseDetector.dir/src/parallelWorldMany.cc.o" "gcc" "CMakeFiles/doseDetector.dir/src/parallelWorldMany.cc.o.d"
+  "/home/ask147/Documents/projects/DoseDetector/src/physicalConstruction.cc" "CMakeFiles/doseDetector.dir/src/physicalConstruction.cc.o" "gcc" "CMakeFiles/doseDetector.dir/src/physicalConstruction.cc.o.d"
+  "/home/ask147/Documents/projects/DoseDetector/src/run.cc" "CMakeFiles/doseDetector.dir/src/run.cc.o" "gcc" "CMakeFiles/doseDetector.dir/src/run.cc.o.d"
+  "/home/ask147/Documents/projects/DoseDetector/src/runAction.cc" "CMakeFiles/doseDetector.dir/src/runAction.cc.o" "gcc" "CMakeFiles/doseDetector.dir/src/runAction.cc.o.d"
   )
 
 # Targets to which this target links.
