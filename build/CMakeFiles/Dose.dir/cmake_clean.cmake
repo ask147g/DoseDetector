cc41e6ac0e0dd1529e026927c5a2c2a73194fc1f
veoutlo <ask147g@gmail.com>
2023-07-03 10:11:12 +0300

    deleted

--- build/CMakeFiles/Dose.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Dose"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Dose.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
